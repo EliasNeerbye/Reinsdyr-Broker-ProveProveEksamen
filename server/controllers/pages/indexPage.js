73ce1cc01e76dad3f869310be2d148c90b2c637b
EliasNeerbye <eliasnee@afk.no>
2025-02-26 09:44:00 +0100

    Updated home page.

--- server/controllers/pages/indexPage.js
+++ server/controllers/pages/indexPage.js
@@ -6,53 +6,14 @@ const Beiteområde = require("../../models/Beiteområde");
 const indexPage = async (req, res) => {
     try {
         const searchOptions = new Set([...Eier.schema.path("kontaktspråk").enumValues]);
-        let userData = null;
-
-        if (req.isAuthenticated) {
-            const user = await Eier.findById(req.session.userId).populate({
-                path: "flokker",
-                populate: { 
-                    path: "reinsdyr",
-                    model: "Reinsdyr"
-                }
-            });
-
-            if (user) {
-                userData = {
-                    navn: user.navn,
-                    flokker: user.flokker.map(flokk => ({
-                        id: flokk._id,
-                        navn: flokk.flokkNavn,
-                        serienummer: flokk.flokkSerienummer,
-                        merkeNavn: flokk.merkeNavn,
-                        merkeBildelenke: flokk.merkeBildelenke,
-                        reinsdyrCount: flokk.reinsdyr ? flokk.reinsdyr.length : 0,
-                        reinsdyr: flokk.reinsdyr ? flokk.reinsdyr.map(rein => ({
-                            id: rein._id,
-                            navn: rein.navn,
-                            serienummer: rein.serienummer,
-                            fødselsdato: rein.fødselsdato ? new Date(rein.fødselsdato).toLocaleDateString() : "Ukjent"
-                        })) : []
-                    }))
-                };
-            }
-
-            user.flokker.forEach((flokk) => {
-                searchOptions.add(flokk.flokkNavn);
-                flokk.reinsdyr.forEach((reinsdyr) => {
-                    searchOptions.add(reinsdyr.navn);
-                });
-            });
-        }
 
         const pageData = createPageData(req, {
             title: "Home",
             cssLinks: ["/css/home.css"],
-            scriptLinks: ["/js/searchBar.js", "/js/navFunctionality.js", "/js/index.js"],
+            scriptLinks: ["/js/searchBar.js", "/js/navFunctionality.js"],
         });
 
         pageData.searchOptions = Array.from(searchOptions);
-        pageData.userData = userData;
 
         res.render("index", pageData);
     } catch (error) {
