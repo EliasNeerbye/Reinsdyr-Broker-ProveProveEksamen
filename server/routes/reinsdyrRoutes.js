d3aaf7ab1f28e40417a9ab3223367ac139b87393
EliasNeerbye <eliasnee@afk.no>
2025-02-26 09:30:33 +0100

    Fix

--- server/routes/reinsdyrRoutes.js
+++ server/routes/reinsdyrRoutes.js
@@ -3,11 +3,7 @@ const router = require("express").Router();
 const { sessionNotExists } = require("../middleware/sessionExists");
 
 const registerRein = require("../controllers/reinsdyr/registerReinController");
-const editRein = require("../controllers/reinsdyr/editReinController");
-const deleteRein = require("../controllers/reinsdyr/deleteReinController");
 
 router.post("/registerRein", sessionNotExists, registerRein);
-router.put("/editRein", sessionNotExists, editRein);
-router.delete("/deleteRein", sessionNotExists, deleteRein);
 
 module.exports = router;
